52cc62c9a4b4c683ee0f8482987472ff8f17171d
Samuel Faucher <sn.faucher@gmail.com>
2017-05-28 15:42:07 -0400

    adds Google Analytics

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {BrowserRouter as  Router, Route, Switch} from 'react-router-dom';
-
 import Home from './home/Home';
 import About from './About';
 import NotFound from './NotFound';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-99981643-1');
+
+
+
+
+function logPageView() {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 const Routes = props => (
-    <Router {...props}>
+    <Router {...props} onUpdate={logPageView}>
         <div>
             <Switch>
                 <Route exact path="/" component={Home}/>
