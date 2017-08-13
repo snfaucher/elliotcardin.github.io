12669bd32cd6865d5cebc3b980944471541ac9cf
Samuel Faucher <sn.faucher@gmail.com>
2017-08-12 23:39:03 -0400

    fix react-snapshot integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+import {render} from 'react-snapshot';
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 import "antd/dist/antd.css";
 import './index.css';
-
+window.matchMedia = window.matchMedia || function() {
+        return {
+            matches : false,
+            addListener : function() {},
+            removeListener: function() {}
+        };
+    };
+global.matchMedia = window.matchMedia;
 render(
     <Routes />,
-    document.getElementById('root'));
+    document.getElementById('root')
+);
 
 registerServiceWorker();
